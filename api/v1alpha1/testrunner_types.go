7a7305bb051d91d36abfb975f859c5281681ef54
KELSON MARTINS <KMartins@ie.ibm.com>
2023-05-09 18:43:45 +0100

    initial commit

---
+++ api/v1alpha1/testrunner_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// TestRunnerSpec defines the desired state of TestRunner
+type TestRunnerSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of TestRunner. Edit testrunner_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// TestRunnerStatus defines the observed state of TestRunner
+type TestRunnerStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// TestRunner is the Schema for the testrunners API
+type TestRunner struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   TestRunnerSpec   `json:"spec,omitempty"`
+	Status TestRunnerStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// TestRunnerList contains a list of TestRunner
+type TestRunnerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []TestRunner `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&TestRunner{}, &TestRunnerList{})
+}
