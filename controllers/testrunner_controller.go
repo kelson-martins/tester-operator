7b0106ab87184779d484c0aa35f01f3f5197cd0a
KELSON MARTINS <KMartins@ie.ibm.com>
2023-05-16 19:03:27 +0100

    swapping CRD

--- controllers/testrunner_controller.go
+++ controllers/testrunner_controller.go
@@ -1,12 +1,9 @@
 /*
 Copyright 2023.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,9 +32,9 @@ type TestRunnerReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=operator.kelson.com,resources=testrunners,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=operator.kelson.com,resources=testrunners/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=operator.kelson.com,resources=testrunners/finalizers,verbs=update
+//+kubebuilder:rbac:groups=operator.anderson.com,resources=testrunners,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=operator.anderson.com,resources=testrunners/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=operator.anderson.com,resources=testrunners/finalizers,verbs=update
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
